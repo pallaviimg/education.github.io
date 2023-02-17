a7dee2ccd2b26d95b87773c84fa671298433e912
pallaviimg <122247897+pallaviimg@users.noreply.github.com>
2023-02-17 19:37:50 +0530

    final push

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,9 @@ $(function(){
     $("#header").load("header.html"); 
     // $("#footer").load("footer.html"); 
 });
+
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('.preloader').fadeOut('active');
+    }, 1200);
+});
